7b5205ee339764a0dda0d457b227c99636f5a26d
vivienneobrien <vxo910@cs.bham.ac.uk>
2021-05-08 11:23:12 +0100

    Added h2 of landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,28 +2,39 @@ import React from "react";
 import Lottie from "react-lottie";
 import animationData from "../animations/landinganimation/data";
 import { makeStyles } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme) =>({
-
-}));
-
+const useStyles = makeStyles((theme) => ({}));
 
 const LandingPage = () => {
+  const classes = useStyles();
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container justify="flex-end" alignItems="center" direction="row">
+          <Grid item>
+            <Typography variant="h2" align="left">
+              Demogapics is bridging the 
+              <br /> gap between minority groups
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+export default LandingPage;
 
-    const classes = useStyles();
-    const defaultOptions = {
-        loop: true,
-        autoplay: true, 
-        animationData: animationData,
-        rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
-        }
-      };
-    return(
-        <div>
-        <Lottie options={defaultOptions} height={"100%"} width={"100%"} ></Lottie>
-        </div>
-    )
-}
-
-export default LandingPage
